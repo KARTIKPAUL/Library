dcffd2433ab216ba4dcd4c13aaf6d1755d4424fe
Parth Sharma <parthsharma16off@gmail.com>
2025-01-16 18:53:37 +0530

    addd latest to parth branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from "react";
 import MainNavbar from "./Component/Navbar/MainNavbar";
 import TestimonialsOne from "./Component/Testimonials/TestimonialsOne";
@@ -11,38 +10,7 @@ function App() {
     <>
      <MainNavbar />
      <TestimonialsOne />
-=======
-import { useState } from 'react';
-import './App.css';
-import SideBarOne from "./Component/Carrousel/SideBarOne";
-import SideBarTwo from './Component/Carrousel/SideBarTwo';
-import SideBarThree from './Component/Carrousel/SideBarThree';
-import SideBarFour from './Component/Carrousel/SideBarFour';
-import SideBarFive from './Component/Carrousel/SideBarFive';
-import SideBarSix from './Component/Carrousel/SideBarSix';  
-import SideBarSeven from './Component/Carrousel/SideBarSeven';
-import SideBarEight from './Component/Carrousel/SideBarEight';  
-import SideBarNine from './Component/Carrousel/SideBarNine';
-import SideBarTen from './Component/Carrousel/SideBarTen';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <SideBarOne />
-      <SideBarTwo />
-      <SideBarThree />
-      <SideBarFour />
-      <SideBarFive />
-      <SideBarSix /> 
-      <SideBarSeven />
-      <SideBarEight /> 
-      <SideBarNine />
-      <SideBarTen />
->>>>>>> origin/Parth
-    </>
+     </>
   );
 }
 
-export default App;
